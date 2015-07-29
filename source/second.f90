646ad0555d5762d6373a998b290081d2a6fb9652
Alexander Soudackov (shscluster) <asouda@illinois.edu>
2015-07-29 16:46:30 -0500

    removed standalone version of second.f90 - function second is now in the module timers

--- source/second.f90
+++
@@ -1,9 +0,0 @@
-function second() result(tic)
-   implicit none
-   real(4), dimension(2) :: time
-   real(4) :: etime
-   real(8) :: tic
-   tic = dble(etime(time))
-   !tic = 0.d0
-   return
-end function second
