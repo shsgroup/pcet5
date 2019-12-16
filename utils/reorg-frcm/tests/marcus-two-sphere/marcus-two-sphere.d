1d610e5408a614b6936744da9850f5e34971b426
Alexander Soudackov <as3586@yale.edu>
2019-12-16 14:08:20 -0500

    final Marcus two-sphere test example for EREORG

--- utils/reorg-frcm/tests/marcus-two-sphere/marcus-two-sphere.d
+++ utils/reorg-frcm/tests/marcus-two-sphere/marcus-two-sphere.d
@@ -1,8 +1,8 @@
 XYZ
 Marcus Fe(2+)-Fe(3+) system
 
-Fe     0.0     0.0     -2.5
-Fe     0.0     0.0      2.5
+Fe     0.0     0.0     -500.0
+Fe     0.0     0.0      500.0
 
 ATOMIC CHARGES
   2.0    3.0   2.0   3.0
